db95424abcfa60d1b9ede180aa1810948d17a9ce
Johannes Emmerling <johannes.emm@gmail.com>
2020-02-17 14:07:34 +0100

    Remove dependency on build folder for setdep

--- functions/add_historical_values.R
+++ functions/add_historical_values.R
@@ -26,13 +26,25 @@ add_historical_values <- function(variable, varname=deparse(substitute(variable)
     if(verbose) print(paste0("Historical values added for '", varname, "'."))
     item <- grep(paste(paste0("^", tolower(varname), valid_suffix), collapse = '|'), .gdx$parameters$name, value = TRUE) #use grep with ^ to have them start by varname
     for(.item in item){.hist_single <- as.data.table(.gdx[.item]); .hist_single$file <- gsub(paste0(tolower(varname), "_"), "", .item); if(.item==item[1]){.hist <- .hist_single}else{.hist <- rbind(.hist,.hist_single)} } 
+    
     #get set dependency based on /build/ folder
-    .gdxiso3 <- gdx(file.path(witch_folder, "input", "build", basename(.gdxname))); 
-    colnames(.hist) <- c(colnames(.gdxiso3[item[1]]), "file")	
-    #in built global data have set "global", but in input folder it gets converted to iso3, so:
-    colnames(.hist) <- gsub("global", "iso3", colnames(.hist)) #add "World" if no country level data but global
-    if(!("iso3" %in% colnames(.hist))){.hist$n = "World"}else{colnames(.hist) <- gsub("iso3", "n", colnames(.hist))}
-    setnames(.hist, "year", "t")
+    use_build <- F; if(use_build){
+      .gdxiso3 <- gdx(file.path(witch_folder, "input", "build", basename(.gdxname))); 
+      print(str(.hist)); print(str(variable)); print(str(.gdxiso3[item[1]]))
+      colnames(.hist) <- c(colnames(.gdxiso3[item[1]]), "file")	
+      #in built global data have set "global", but in input folder it gets converted to iso3, so:
+      colnames(.hist) <- gsub("global", "iso3", colnames(.hist)) #add "World" if no country level data but global
+      if(!("iso3" %in% colnames(.hist))){.hist$n = "World"}else{colnames(.hist) <- gsub("iso3", "n", colnames(.hist))}
+      setnames(.hist, "year", "t")
+      if(verbose) print(.hist)
+    }else{
+    #try to get dependency from variable itself
+      setdep <- setdiff(names(variable), c("n", "file", "pathdir", "t", "value"))
+      setdep <- c(setdep, "t")
+      setnames(.hist, paste0("V", seq(1:length(setdep))), setdep)
+      if(verbose) print(.hist)
+    }
+    
     #adjust time unit to model
     .hist$t <- yeartot(.hist$t)
     t_historical<-unique(.hist$t)

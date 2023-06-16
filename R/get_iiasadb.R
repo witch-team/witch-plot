f4de1837bb954818469b24f8a351a04fc7a20530
Johannes Emmerling <johannes.emm@gmail.com>
2023-06-16 11:26:24 +0200

    fix inequality plots; iiasadb for any database

--- R/get_iiasadb.R
+++ R/get_iiasadb.R
@@ -1,8 +1,7 @@
-#First install these four packaged if not yet available
+#First install these packages if not yet available (for standalone mode)
 #require(tidyverse)
-#require(arrow)
 #require(reticulate)
-#require(plotly)
+#require(yaml)
 
 #Function to get IIASAdb variable
 get_iiasadb <- function(database="ar6-public", varlist="Emissions|CO2", varname=NULL, region="World", show_variables = F){
@@ -10,24 +9,33 @@ get_iiasadb <- function(database="ar6-public", varlist="Emissions|CO2", varname=
   pyam <- import("pyam", convert = FALSE)
   #if private databases provide credentials
   if(file.exists("iiasa_credentials.yml")){
+    require(yaml)
     iiasa_credentials <- yaml::read_yaml("iiasa_credentials.yml") # two lines with username: and password:
     pyam$iiasa$set_config(iiasa_credentials$username, iiasa_credentials$password)
   }
   #show variables in case
-  if(show_variables) print(paste(variable=py_to_r(pyam$iiasa$Connection(database)$variables())))
-  assign("variables", as.data.frame(py_to_r(pyam$iiasa$Connection(database)$variables())), envir = .GlobalEnv)
-  ar6_data <- pyam$read_iiasa(database, model='*', scenario="*", variable=varlist, region=region, meta=1)
-  #as_pandas concatenates data and meta into a pandas DF (meta_cols = TRUE adds all meta data)
-  iiasadb_df <- ar6_data$as_pandas(meta_cols = c("Ssp_family", "Policy_category", "Policy_category_name", "Category", "IMP_marker"))
-  #pandas to R data frame
-  iiasadb_df <- py_to_r(iiasadb_df)
-  #all categories are lists, convert to simple vectors
-  Policy_category <- data.frame(Policy_category=unlist(iiasadb_df$Policy_category))
-  Policy_category_name <- data.frame(Policy_category_name=unlist(iiasadb_df$Policy_category_name))
-  Category <- data.frame(Category=unlist(iiasadb_df$Category))
-  iiasadb_df <- iiasadb_df %>% select(-c("Policy_category", "Policy_category_name", "Category"))
-  iiasadb_df <- cbind(iiasadb_df, Policy_category, Policy_category_name, Category)
-  iiasadb_df <- iiasadb_df %>% dplyr::filter(Category!="failed-vetting" & Category!="NaN" & Category!="no-climate-assessment")
+  if(show_variables) print(py_to_r(pyam$iiasa$Connection(database)$variables()))
+  assign("iiasadb_variables_available", as.data.frame(py_to_r(pyam$iiasa$Connection(database)$variables())), envir = .GlobalEnv)
+  print("Available_Connections:"); print(py_to_r(pyam$iiasa$Connection(database)$valid_connections))
+  iiasadb_data <- pyam$read_iiasa(database, model='*', scenario="*", variable=varlist, region=region, meta=1)
+  #If AR6, also add meta categories and other meta data
+  if(database == "ar6_public" ){
+     #as_pandas concatenates data and meta into a pandas DF (meta_cols = TRUE adds all meta data)
+    iiasadb_df <- iiasadb_data$as_pandas(meta_cols = c("Ssp_family", "Policy_category", "Policy_category_name", "Category", "IMP_marker"))
+    #pandas to R data frame
+    iiasadb_df <- py_to_r(iiasadb_df)
+    #all categories are lists, convert to simple vectors
+    Policy_category <- data.frame(Policy_category=unlist(iiasadb_df$Policy_category))
+    Policy_category_name <- data.frame(Policy_category_name=unlist(iiasadb_df$Policy_category_name))
+    Category <- data.frame(Category=unlist(iiasadb_df$Category))
+    iiasadb_df <- iiasadb_df %>% select(-c("Policy_category", "Policy_category_name", "Category"))
+    iiasadb_df <- cbind(iiasadb_df, Policy_category, Policy_category_name, Category)
+    iiasadb_df <- iiasadb_df %>% dplyr::filter(Category!="failed-vetting" & Category!="NaN" & Category!="no-climate-assessment")
+  } 
+  else
+  {
+    iiasadb_df <- py_to_r(iiasadb_data$as_pandas())
+  }
   if(!is.null(varname)) iiasadb_df <- iiasadb_df %>% mutate(variable = plyr::mapvalues(variable, from = varlist, to = varname))
   return(iiasadb_df)
 }

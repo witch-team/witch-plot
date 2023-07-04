5d6ef9e6b87d3791bdf0009d3be296db075dcb9d
Laurent Drouet <ldrouet@pm.me>
2023-07-04 14:57:16 +0200

    ylim rather adds yintercept=0

--- gdxcompaR/witch/ui.R
+++ gdxcompaR/witch/ui.R
@@ -21,7 +21,10 @@ shinyUI(fluidPage(pageWithSidebar(
     uiOutput("select_scenarios"),
     uiOutput("select_regions"),
     div(style="display:inline-block",checkboxInput("add_historical", "Add historical", value = T)),
-    div(style="display:inline-block",checkboxInput("ylim_zero", "ymin=0", value = F)),
+    div(style="display:inline-block",
+        checkboxInput("ylim_zero",
+                      "ymin=0",
+                      value = FALSE)),
     div(style="display:inline-block",radioButtons("field", "", choiceNames = c("l","up","lo"), choiceValues = c("l","up","lo"), inline = TRUE)),
     div(style="display:inline-block",actionButton("button_saveplotdata", "Save Plot"))
 ),
